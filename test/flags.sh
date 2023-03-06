17f69b41360ab4eb2f449ee29e8c87eb07518de9
Dan Kolz <dan@emergentideas.com>
2023-03-06 10:34:33 -0600

    captured some learning in using name arguments using getopts

--- test/flags.sh
+++ test/flags.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+echo "start $#"
 while getopts u:a:f: flag
 do
-	echo ${flag}
+#	echo ${flag}
     case "${flag}" in
         u) username=${OPTARG};;
         a) age=${OPTARG};;
@@ -10,9 +11,17 @@ do
     esac
 done
 
+echo "optind $OPTIND"
 echo "username: ${username}"
 echo "age: ${age}"
 echo "fullname: ${fullname}"
+
+# makes it so that only the arguments to the program not picked up by getopts are $1 $2 $3 etc.
+# this is how you use arguments which are not named parameters
+shift $(expr $OPTIND - 1 )
+
 echo "1: ${1}"
 echo "2: ${2}"
 echo "3: ${3}"
+#printf "%s\n" "$#" 
+echo $#
