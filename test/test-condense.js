f6f6aeb1cd32a906ec5d5cf62ee29089c34b5dba
Dan Kolz <dan@emergentideas.com>
2024-03-21 15:27:48 -0500

    tools to create double density picture elements

--- test/test-condense.js
+++ test/test-condense.js
@@ -1,6 +1,8 @@
 const mocha = require('mocha')
 const assert = require('chai').assert
 const condense = require('../lib/condense-image-variants')
+const parseWebp2xUrl = require('../lib/parse-webp2x-url')
+const generatePictureMarkup = require('../lib/generate-picture-markup')
 
 let data1 = require('../test-data/data1')
 let data2 = require('../test-data/data2')
@@ -15,6 +17,52 @@ describe(" basic tests", function() {
 		let remaining = keys.filter(item => !aggregateBaseName.includes(item))
 
 		assert.equal(remaining.length, 0)
+	})
+	
+	it("url parse", function() {
+		let url = '/empty/4cats.jpg#format=webp2x&width=512&height=256&alt=see%20the%204%20cats'
+		let o = parseWebp2xUrl(url)
+
+		assert.equal(o.url, '/empty/4cats.jpg')
+		assert.equal(o.params.width, '512')
+		assert.equal(o.params.height, '256')
+		assert.equal(o.params.format, 'webp2x')
+		assert.equal(o.params.alt, 'see the 4 cats')
+	})
+
+	it("picture generation", function() {
+		let url = '/empty/4cats.jpg#format=webp2x&width=512&height=256&alt=see%20the%204%20cats'
+		let o = parseWebp2xUrl(url)
+		let html = generatePictureMarkup(o.url, o.params)
+		console.log(html)
+
+	})
+
+	it("picture generation 2", function() {
+		let url = '/empty/4cats.png#format=webp2x&width=512&height=256&alt=see%20the%204%20cats'
+		let o = parseWebp2xUrl(url)
+		o.params.displayWidth = '300px'
+		o.params.height = 200
+		o.params.pictureStyle = 'display: block'
+		o.params.imgStyle = 'display: inline'
+		o.params.pictureClass = 'my-pic'
+		let html = generatePictureMarkup(o.url, o.params)
+		console.log(html)
+
+	})
+
+	it("picture generation 3", function() {
+		let url = '/empty/4cats.png#format=webp2x&width=512&height=256&alt=see%20the%204%20cats'
+		let o = parseWebp2xUrl(url)
+		o.params.displayWidth = '300px'
+		o.params.height = 200
+		o.params.pictureStyle = 'display: block'
+		o.params.imgStyle = 'display: inline'
+		o.params.pictureClass = 'my-pic'
+		delete o.params.format
+		let html = generatePictureMarkup(o.url, o.params)
+		console.log(html)
+
 	})
 
 })
